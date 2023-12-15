c62f290b0529856d15d1cfbfa9a50b5380dab872
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-12-15 15:59:02 +0400

    adding functionality
    adding on/off button, developing responsive. working on js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-window.onload = Init
-function Init(){
-    let displayField = document.getElementById('display')
-    displayField.innerHTML = "Let's Play It!"
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const switchBtn = document.getElementById('switch');
+    const drumBtns = document.querySelectorAll('.drum-pad');
+    const displayField = document.getElementById('display');
+
+    function setInitialState() {
+        displayField.innerHTML = "Let's Play It!";
+        switchBtn.innerHTML = "On";
+        switchBtn.className = "btn btn-outline-success fw-bolder";
+        drumBtns.forEach(button => button.disabled = true);
+    }
+
+    function toggleOnOff() {
+        const isActive = switchBtn.classList.contains('btn-outline-success');
+
+        drumBtns.forEach(button => button.disabled = !isActive);
+        switchBtn.className = isActive ? "btn btn-outline-danger fw-bolder" : "btn btn-outline-success fw-bolder";
+        switchBtn.innerHTML = isActive ? "Off" : "On";
+    }
+    setInitialState();
+    switchBtn.addEventListener('click', toggleOnOff);
+});
+
 
 function play(str){
     let displayField = document.getElementById('display')
@@ -16,4 +34,4 @@ window.document.onkeyup = function(event){
     if(("QWEASDZXC").includes(keyStore)){
         play(keyStore)
     }
-} 
+}
